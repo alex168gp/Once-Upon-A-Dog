5a7a09ae1b2fedc0148f14b4838a0f607c83427d
Соловьёв Алексей Максимович <alex168gp@gmail.com>
2019-08-07 01:06:01 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Once_Upon_A_Dog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
